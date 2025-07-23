737da74be258e77a88e447a05ac5449d25837ce3
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-07-23 19:56:33 +0800

    Update Successor.vue

--- src/components/tabs/Successor.vue
+++ src/components/tabs/Successor.vue
@@ -18,8 +18,7 @@ import TDBuyable from '../TDBuyable.vue';
 				v-if="!player.upgrades['25']"
 			>
 				后继x{{ format(feature.SUCCESSOR.successorBulk())
-				}}
-				<sup v-if="feature.SUCCESSOR.successorPow().gt(1)">{{ format(feature.SUCCESSOR.successorPow()) }}</sup>
+				}}<sup v-if="feature.SUCCESSOR.successorPow().gt(1)">{{ format(feature.SUCCESSOR.successorPow()) }}</sup>
 				<span v-if="BUYABLES.lock('11').unlocked"
 					>(自动{{ formatWhole(feature.SUCCESSOR.autoSuccessPerSecond()) }}/s)</span
 				>
