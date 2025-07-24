44e029c82a35d0cb14eb889a4a3473229d1acdbe
VeryrrDefine <1308061599@qq.com>
2025-07-24 17:09:49 +0800

    eee

--- src/components/TDUpgrade.vue
+++ src/components/TDUpgrade.vue
@@ -4,20 +4,19 @@ import { player } from '@/core/save';
 const props = defineProps<{
 	upgid: keyof typeof player.upgrades;
 }>();
-function curupg(){
-  return 'upgrade'; 
-}
+// code...
 </script>
 
 <template>
 	<td v-if="UPGRADES.lock(upgid).show">
 		<div
 			class="upgrade"
-			@click="UPGRADES.buy(upgid)"
+			@mousedown="UPGRADES.buy(upgid)"
 			v-html="UPGRADES.singleHTML(upgid)"
 		></div>
 	</td>
 </template>
 
 <style scoped>
+/* code... */
 </style>
