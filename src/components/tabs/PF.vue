c628885985dd60e1b2de09ef13dcc93271827190
0i00000000a7 <byd255brkdn@hotmail.com>
2025-07-29 21:02:43 +0800

    bbifeihg

--- src/components/tabs/PF.vue
+++ src/components/tabs/PF.vue
@@ -19,9 +19,9 @@ import type { PrimeFactorTypes } from '@/core/save';
 					<span style="color: #cc33ff; font-weight: bold"
 						>{{ pf
 						}}<sup>{{
-							player.buyables[('pf' + pf) as PrimeFactorTypes].add(
+							formatWhole(player.buyables[('pf' + pf) as PrimeFactorTypes].add(
 								buyables?.['pf' + pf]?.more?.() ?? 0,
-							)
+							))
 						}}</sup></span
 					><span v-if="pf != '19'"> × </span> </span
 				>)<span
