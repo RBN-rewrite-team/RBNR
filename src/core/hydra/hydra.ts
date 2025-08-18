d78c3569080a28535ee02ad941f8f4030b2eac35
VeryrrDefine <1308061599@qq.com>
2025-08-19 01:45:06 +0800

    fixed unexcepted infinity

--- src/core/hydra/hydra.ts
+++ src/core/hydra/hydra.ts
@@ -107,7 +107,7 @@ export const Hydra = {
 				return '×' + format(this.effect());
 			}
 			effect(): Decimal {
-				return player.hydra.power.log10().max(500).div(500);
+				return player.hydra.power.clampMin(1).log10().max(500).div(500);
 			}
 			currency: Currencies = Currencies.HYDRA_POWER;
 		})(),
