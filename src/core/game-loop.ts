984e06b2d4e9fc0feb37680016571f107d8832ca
VeryrrDefine <1308061599@qq.com>
2025-08-21 12:43:41 +0800

    wtf

--- src/core/game-loop.ts
+++ src/core/game-loop.ts
@@ -215,7 +215,7 @@ export function simulate(diff: number) {
 		}
 		let base = feature.Ordinal.base();
 		if (player.ordinal.number.gte(base.tetrate(base.toNumber()))) player.help.epsilon = true;
-		if ([0, 2, 4, 5, 6, 9, 10, 12, 13].includes(player.currentTab)) {
+		if ([0, 2, 4, 5, 9, 10, 12, 13].includes(player.currentTab)) {
 			player.currentTab = 14;
 		}
 	}
