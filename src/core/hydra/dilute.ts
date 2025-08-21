d3aec3c0c25d1b095025ac1eed36a99f34acd3ff
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-08-21 08:50:20 +0800

    g

--- src/core/hydra/dilute.ts
+++ src/core/hydra/dilute.ts
@@ -134,7 +134,7 @@ export const Dilute = {
 			reqDescription: '在满级稀释2的稀释中达到0.14轮回效果且 & 19000九头蛇溶液',
 			requirement: new Decimal(0.14),
 			get canDone() {
-				return player.hydra.dilute.inDilute && (diluteAmount(1) as number >= 10) && Hydra.prestigeEff(3).gte(0.14) && (player.hydra.dilute.solution > 19000);
+				return player.hydra.dilute.inDilute && (diluteAmount(1) as number >= 10) && Hydra.prestigeEff(3).gte(0.14) && (player.hydra.dilute.solution >= 19000);
 			},
 			show: true,
 			currency: '',
