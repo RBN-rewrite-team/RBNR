c0d76811967a8f02f4c639ff9d4dfc42b5df28aa
VeryrrDefine <1308061599@qq.com>
2025-08-23 23:12:04 +0800

    added theories cost, non UIs

--- src/core/nonrecu/studies.ts
+++ src/core/nonrecu/studies.ts
@@ -1,6 +1,7 @@
 import Decimal from 'break_eternity.js';
 import { player } from '../save';
 import { Hydra } from '../hydra/hydra';
+import { Currencies, getCurrency } from '../currencies';
 
 interface StudyConfig {
 	id: string;
@@ -69,4 +70,56 @@ export function buyStudies(id: number) {
 
 export function getTotalTheories() {
 	return player.nonrecu.theories.reduce((a, b) => a.add(b));
+}
+export function theoriesCost(id: 0 | 1 | 2) {
+	switch (id) {
+		case 0:
+			if (player.nonrecu.theories[0].lte(10)) {
+				return player.nonrecu.theories[0].pow10().pow10();
+			} else if (player.nonrecu.theories[0].lte(70)) {
+				return Decimal.tetrate(
+					10,
+					player.nonrecu.theories[0].mul(0.05).add(2.5).toNumber(),
+				);
+			} else {
+				return Decimal.tetrate(
+					10,
+					5,
+					player.nonrecu.theories[0].pow(2).mul(100).sub(489990),
+				);
+			}
+		case 1:
+			return player.nonrecu.theories[1].pow10().mul(1e4);
+		case 2:
+			return player.nonrecu.theories[2].pow_base(5);
+		default:
+			let a: never = id;
+	}
+	return new Decimal(1 / 0);
+}
+export function addTheories(id: 0 | 1 | 2) {
+	switch (id) {
+		case 0:
+			if (player.hydra.power.gte(theoriesCost(0))) {
+				player.hydra.power = player.hydra.power.sub(theoriesCost(0));
+				player.nonrecu.theories[0] = player.nonrecu.theories[0].add(1);
+			}
+			break;
+		case 1:
+			if (getCurrency(Currencies.SOLUTION).gte(theoriesCost(1))) {
+				player.hydra.dilute.solutionCost =
+					player.hydra.dilute.solutionCost +
+					theoriesCost(1).clampMax(Number.MAX_VALUE).toNumber();
+				player.nonrecu.theories[1] = player.nonrecu.theories[1].add(1);
+			}
+			break;
+		case 2:
+			if (player.nonrecu.power.gte(theoriesCost(2))) {
+				player.nonrecu.power = player.nonrecu.power.sub(theoriesCost(2));
+				player.nonrecu.theories[2] = player.nonrecu.theories[2].add(1);
+			}
+			break;
+		default:
+			let a: never = id;
+	}
 }
