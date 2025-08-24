115f143578e15c5dcfa3272def8f875e8263b715
VeryrrDefine <1308061599@qq.com>
2025-08-24 11:04:47 +0800

    total

--- src/components/tabs/nonrecursion/NonRecursionTab.vue
+++ src/components/tabs/nonrecursion/NonRecursionTab.vue
@@ -3,6 +3,8 @@ import TRMilestone from '@/components/TRMilestone.vue';
 import StudyTree from './StudyTree.vue';
 import { formatWhole } from '@/utils/format';
 import { addTheories, theoriesCost } from '@/core/nonrecu/studies';
+import { getTotalTheories } from '@/core/nonrecu/total-theories';
+import { Currencies, getCurrency } from '@/core/currencies';
 
 </script>
 
@@ -11,7 +13,7 @@ import { addTheories, theoriesCost } from '@/core/nonrecu/studies';
     <table align="center">
       <TRMilestone id="nonrec_1"></TRMilestone>
     </table>
-
+    <p>你一共有<b style="color: #c98300; font-size: 30px">{{ formatWhole(getTotalTheories()) }}</b>非递归理论，还剩下{{ formatWhole(getCurrency(Currencies.NRT)) }}。</p>
     <StudyTree />
   </div>
 </template>
