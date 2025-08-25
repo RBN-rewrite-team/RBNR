0941dd9d0630354c8ff01dced7970d1623255559
VeryrrDefine <1308061599@qq.com>
2025-08-25 23:20:08 +0800

    fixed display

--- src/components/tabs/plot/PlotTab.vue
+++ src/components/tabs/plot/PlotTab.vue
@@ -7,12 +7,12 @@ import PlotNode from './PlotNode.vue';
 <template>
   <div class="main">
     <h1>剧情设定</h1>
-    <div style="position: relative;">
+    <div style="position: fixed;">
       <PlotNode style="top: 10px; left: 30px;" @click="temp.plotdisplay = 1"><vue-latex expression="-\varepsilon"></vue-latex></PlotNode>
-      <div class="node" style="top: -17.2px;left: 93px;background-color: var(--color);width: 100px;height: 1px;"></div>
-      <PlotNode style="top: -43.2px;left: 202.5px;">未解锁</PlotNode>
-      <div class="node" style="top: -70.2px;left: 265px;background-color: var(--color);width: 100px;height: 1px;"></div>
-      <PlotNode style="top: -97.2px;left: 375px;">未解锁</PlotNode>
+      <div class="node" style="top: 35.7px;left: 93px;background-color: var(--color);width: 100px;height: 1px;"></div>
+      <PlotNode style="top: 10px;left: 202.5px;">未解锁</PlotNode>
+      <div class="node" style="top: 35.7px;left: 265px;background-color: var(--color);width: 100px;height: 1px;"></div>
+      <PlotNode style="top: 10px;left: 375px;">未解锁</PlotNode>
       
     </div>
   </div>
@@ -20,6 +20,6 @@ import PlotNode from './PlotNode.vue';
 
 <style lang="css" scoped>
 .node {
-  position: relative;
+  position: absolute;
 }
 </style>
