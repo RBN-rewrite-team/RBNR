aade70dca07d76c8f8c7c1d1eb152965242675a2
VeryrrDefine <1308061599@qq.com>
2025-08-25 23:23:19 +0800

    plot display 2

--- src/components/tabs/plot/PlotTab.vue
+++ src/components/tabs/plot/PlotTab.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { temp } from '@/core/temp-data';
 import PlotNode from './PlotNode.vue';
-
+import { unlockedPlots } from '@/core/plot';
+function enterPlot(i: number){
+  if (unlockedPlots()>=i){temp.plotdisplay=i}
+}
 </script>
 
 <template>
@@ -10,7 +13,8 @@ import PlotNode from './PlotNode.vue';
     <div style="position: fixed;">
       <PlotNode style="top: 10px; left: 30px;" @click="temp.plotdisplay = 1"><vue-latex expression="-\varepsilon"></vue-latex></PlotNode>
       <div class="node" style="top: 35.7px;left: 93px;background-color: var(--color);width: 100px;height: 1px;"></div>
-      <PlotNode style="top: 10px;left: 202.5px;">未解锁</PlotNode>
+      <PlotNode style="top: 10px;left: 202.5px;" @click="enterPlot(2)">
+        <template v-if="unlockedPlots()>=2"><vue-latex expression="0"></vue-latex></template><template v-else>未解锁</template></PlotNode>
       <div class="node" style="top: 35.7px;left: 265px;background-color: var(--color);width: 100px;height: 1px;"></div>
       <PlotNode style="top: 10px;left: 375px;">未解锁</PlotNode>
       
