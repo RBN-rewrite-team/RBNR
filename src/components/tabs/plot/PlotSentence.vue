395dc709ef238b2fb55fce5bff138e835820e73a
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-08-25 23:14:27 +0800

    Numerorum帅气大头照

--- src/components/tabs/plot/PlotSentence.vue
+++ src/components/tabs/plot/PlotSentence.vue
@@ -8,9 +8,7 @@ const props = defineProps<{
 
 <template>
 	<tr><td><div align="center" style="position: relative; width: 100%">
-		<div class="chara_pic">
-			没图片<br>
-			找群主
+		<div class="chara_pic" :style="{ 'background-image': image }">
 		</div>
 		<span class="chara_name" v-html="name" />
 		<div class="main" v-html="text" />
@@ -36,7 +34,8 @@ const props = defineProps<{
 	box-shadow: 0px 0px 10px 10px var(--suptitle-color);
 }
 .chara_pic {
-	background-color: rgba(127, 127, 127, 1);
+	background-image: url(./plot_image/placeholder.png);
+	background-size: cover;
 	color: var(--color);
 	font-size: 24px;
 	position: absolute;
