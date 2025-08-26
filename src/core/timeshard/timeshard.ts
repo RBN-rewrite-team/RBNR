cb1c19cbf7f2ce5c1512b28589f4bd687c8030e2
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-08-26 15:14:31 +0800

    next修复

--- src/core/timeshard/timeshard.ts
+++ src/core/timeshard/timeshard.ts
@@ -8,23 +8,26 @@ export const TimeShard = {
 		if (id == 0) {
 			if (dn >= player.timeshard.cd[0]) {
 				const r = Math.floor(Math.random() * 40) + 10;
-				player.timeshard.value += r;
+				player.timeshard.value += player.timeshard.next[0];
 				player.timeshard.cd[0] = dn + 60 * 60 * 1000;
-				player.timeshard.last[0] = r;
+				player.timeshard.last[0] = player.timeshard.next[0];
+				player.timeshard.next[0] = r;
 			}
 		} else if (id == 1) {
 			if (dn >= player.timeshard.cd[1]) {
 				const r = Math.floor(Math.random() * 320) + 80;
-				player.timeshard.value += r;
+				player.timeshard.value += player.timeshard.next[1];
 				player.timeshard.cd[1] = dn + 24 * 60 * 60 * 1000;
-				player.timeshard.last[1] = r;
+				player.timeshard.last[1] = player.timeshard.next[1];
+				player.timeshard.next[1] = r;
 			}
 		} else if (id == 2) {
 			if (dn >= player.timeshard.cd[2]) {
 				const r = Math.floor(Math.random() * 4000) + 1000;
-				player.timeshard.value += r;
+				player.timeshard.value += player.timeshard.next[2];
 				player.timeshard.cd[2] = dn + 7 * 24 * 60 * 60 * 1000;
-				player.timeshard.last[2] = r;
+				player.timeshard.last[2] = player.timeshard.next[2];
+				player.timeshard.next[2] = r;
 			}
 		}
 	},
