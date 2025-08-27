e6247b7dcc3f6024e9a65484c3388bbac22d40cf
seanxlx2011 <seanxlx@foxmail.com>
2025-08-27 09:42:17 +0800

    seanxlx

--- src/core/nonrecu/non-recursion-challenges.ts
+++ src/core/nonrecu/non-recursion-challenges.ts
@@ -7,12 +7,12 @@ export const NONREC_CHALS: SingleChallenge[] = [
 	{
 		name: '突变',
 		descEasy:
-			'锁定在[10,10,10,10,10,9.75,true,true,false]中。保留U5-S-9，但是你不免疫朊病毒，朊病毒增益反转且在你第一次推演后以100*10^完成次数倍每秒的速度增长，如果朊病毒超越你的推演次数，朊病毒重置为1并进行一次稀释级别的重置。',
+			'锁定在[10,0.4667,10,10,10,9,true,false,false]中。溶剂2影响稀释升级。保留U5-S-9，但是你不免疫朊病毒，朊病毒增益反转且在你第一次推演后以100*10^完成次数倍每秒的速度增长，如果朊病毒超越你的推演次数，朊病毒重置为1并进行一次稀释级别的重置。',
 		descHard:
-			'锁定在[10,10,10,10,10,9.75,true,true,false]中。保留U5-S-9，但是你不免疫朊病毒，朊病毒增益反转且在你第一次推演后以100*10^完成次数倍每秒的速度增长，如果朊病毒超越你的推演次数，朊病毒重置为1并进行一次稀释级别的重置。',
+			'锁定在[10,0.4667,10,10,10,9,true,false,false]中。溶剂2影响稀释升级。保留U5-S-9，但是你不免疫朊病毒，朊病毒增益反转且在你第一次推演后以100*10^完成次数倍每秒的速度增长，如果朊病毒超越你的推演次数，朊病毒重置为1并进行一次稀释级别的重置。',
 		loop() {
 			player.hydra.dilute.inDilute = true;
-			player.hydra.dilute.solvent = [10, 10, 10, 10, 10, 9.75, !0, !0, !1];
+			player.hydra.dilute.solvent = [10, 0.4667, 10, 10, 10, 9, !0, !1, !1];
 			if (Dilute.prions().sub(1).gte(player.hydra.deduceOrdinal[0])) {
 				Dilute.diluteReset();
 				player.hydra.dilute.prions = new Decimal(1);
