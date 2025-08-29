71acfe7771c5512efaf88950a64b581f85419f55
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-08-29 13:16:20 +0800

    v

--- src/components/tabs/MiniGame.vue
+++ src/components/tabs/MiniGame.vue
@@ -15,14 +15,37 @@ function spawn(id: number): void {
 <template>
 
     <div class="main">
+      <div style="width: 400px; height: 200px; background-color: grey;">
+            Numerorum<br/>
+            <div style="position: relative; height: 50px; width: 400px; background-color: black">
+				<div align="center" style="font-size: 17px">生命值：{{meBattleInfo().hp}}/{{meBattleInfo().hpMax}}({{Math.ceil(meBattleInfo().hp / meBattleInfo().hpMax * 100)}}%)</div>
+				<div :style="{position: 'absolute', height: '25px', width: meBattleInfo().hp / meBattleInfo().hpMax * 400 + 'px', 'background-color': 'red'}"></div>
+			</div>
+            <table style="width: 100%">
+			<tbody>
+			<tr>
+			<td>当前攻击力: {{ meBattleInfo().atk }}</td>
+            <td>当前防御: {{ meBattleInfo().def }}</td>
+			</tr>
+			<tr>
+			<td>当前LV: {{ currentPlayerLV() }}</td>
+            <td :style="{'background-image': 'linear-gradient(to right, green ' + (player.minigame.xp / nextLVxp() * 100) + '%, black ' + (player.minigame.xp / nextLVxp() * 100) + '%)'}">当前XP: {{ player.minigame.xp }}/{{ nextLVxp() }}</td>
+			</tr>
+			</tbody>
+			<tr>
+			<td>矿石收集：{{player.minigame.ore_gets}}<br>(+{{player.minigame.ore_gets * 0.25}}%全局速度)</td>
+			</tr>
+			<tr>
+			<td><button @click="spawn(0)">Dungeon 1</button></td>
+			</tr>
+			</table>
+        </div>
+      <br>
         <div style="display: flex; flex-direction: row; justify-content: center">
           <button @click="handleKeyPress('up')" class="clickable_button">↑</button>
           <button @click="handleKeyPress('down')" class="clickable_button">↓</button>
           <button @click="handleKeyPress('left')" class="clickable_button">←</button>
           <button @click="handleKeyPress('right')" class="clickable_button">→</button>
-        </div>
-        <div style="display: flex; flex-direction: row; justify-content: center">
-          <button @click="hardResetMiniGame" class="clickable_button">复位</button>
         </div>
         <br />
         <table>
@@ -59,31 +82,6 @@ function spawn(id: number): void {
                 </tr> -->
             </tbody>
         </table>
-        <div style="margin: auto; position: absolute; left: 0%; top: 0%; width: 400px; height: 200px; background-color: grey;">
-            Numerorum<br/>
-            <div style="position: relative; height: 50px; width: 400px; background-color: black">
-				<div align="center" style="font-size: 17px">生命值：{{meBattleInfo().hp}}/{{meBattleInfo().hpMax}}({{Math.ceil(meBattleInfo().hp / meBattleInfo().hpMax * 100)}}%)</div>
-				<div :style="{position: 'absolute', height: '25px', width: meBattleInfo().hp / meBattleInfo().hpMax * 400 + 'px', 'background-color': 'red'}"></div>
-			</div>
-            <table style="width: 100%">
-			<tbody>
-			<tr>
-			<td>当前攻击力: {{ meBattleInfo().atk }}</td>
-            <td>当前防御: {{ meBattleInfo().def }}</td>
-			</tr>
-			<tr>
-			<td>当前LV: {{ currentPlayerLV() }}</td>
-            <td :style="{'background-image': 'linear-gradient(to right, green ' + (player.minigame.xp / nextLVxp() * 100) + '%, black ' + (player.minigame.xp / nextLVxp() * 100) + '%)'}">当前XP: {{ player.minigame.xp }}/{{ nextLVxp() }}</td>
-			</tr>
-			</tbody>
-			<tr>
-			<td>矿石收集：{{player.minigame.ore_gets}}<br>(+{{player.minigame.ore_gets * 0.25}}%全局速度)</td>
-			</tr>
-			<tr>
-			<td><button @click="spawn(0)">Dungeon 1</button></td>
-			</tr>
-			</table>
-        </div>
     </div>
 </template>
 
