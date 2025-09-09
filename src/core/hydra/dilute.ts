40fe06a09a8de46d3d7aadf3f04b937501221c04
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-09-09 12:54:01 +0800

    fix

--- src/core/hydra/dilute.ts
+++ src/core/hydra/dilute.ts
@@ -72,7 +72,7 @@ export function milestoneDut5Eff(): Decimal {
 
 export function milestoneDut6Eff(): Decimal {
 	if (player.hydra.dilute.solution.gte(2050000)) return DC.D_1;
-	return Decimal.log10(player.hydra.dilute.solution.sub(2050000 - 1))
+	return Decimal.log10(player.hydra.dilute.solution.max(2050000).sub(2050000 - 1))
 		.add(1)
 		.clampMin(1)
 		.log10()
