acea434d9e9dbdef3d78a8974cbe4fe2a3f834e5
VeryrrDefine <1308061599@qq.com>
2025-09-15 21:05:01 +0800

    hyw

--- src/core/hydra/dilute.ts
+++ src/core/hydra/dilute.ts
@@ -440,7 +440,7 @@ const Dil = {
 		});
 		MILESTONES.create('dut3', {
 			displayName: 'M-Dilute-3',
-			description: '转生永久不重置任何东西，永久解锁自动转生，(仅在稀释VII)初始解锁所有升级',
+			description: '转生永久不重置任何东西，永久解锁自动转生，(仅在稀释VII)初始显示所有升级',
 			req: true,
 			reqDescription: '在满级稀释2的稀释中达到0.135轮回效果且总计拥有过19000九头蛇溶液',
 			requirement: new Decimal(0.135),
