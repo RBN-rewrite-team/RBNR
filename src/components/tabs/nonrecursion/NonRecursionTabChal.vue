5ed585bec63a8ce61b3380024e0464fa1572c581
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-09-19 21:18:59 +0800

    h

--- src/components/tabs/nonrecursion/NonRecursionTabChal.vue
+++ src/components/tabs/nonrecursion/NonRecursionTabChal.vue
@@ -6,6 +6,7 @@ import { player } from '@/core/save';
 <template>
 	<div class="main nonrec-chals" align="center">
 		在非递归挑战中，溶剂II将增加U5-S系列的价格<br />
+		注意：所有非递归挑战至多完成5次。
 		<table>
 			<tbody>
 				<tr>
