bc94ec5965a6645e2094b320cc58eb78548e4ae2
VeryrrDefine <1308061599@qq.com>
2025-09-20 10:10:48 +0800

    aaa

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -13,8 +13,7 @@ import { Dilute } from '@/core/hydra/dilute.ts';
 import { startGameLoop, stopGameLoop, stopSaveLoop } from '@/core/game-loop';
 import { NON_RECURSIVE } from '@/core/nonrecu/index.ts';
 import ModalService from './Modal.ts';
-import { temp } from '@/core/temp-data.ts';
-import { keyboardEventListener, hardResetMiniGame } from '@/core/minigame/index.ts';
+import { keyboardEventListener } from '@/core/minigame/index.ts';
 
 import App from '@/App.vue';
 
