1c285541196d9bee29294a1b52a0e011f400fa89
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-09-23 22:57:31 +0800

    Revert "Seanxlx"

--- src/components/tabs/group-1/MiniGame.vue
+++ src/components/tabs/group-1/MiniGame.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-//@ts-nocheck
 import {
 	currentPlayerLV,
 	nextLVxp,
@@ -102,7 +101,6 @@ function exitEditor() {
 }
 function openCore() {
 	temp.openingCore = !temp.openingCore;
-	//@ts-ignore
 	if(temp.openingCore && temp.coreViewEquipment.rarity == undefined)
 	{
 		if(player.minigame.storeEquipments.length == 0)
@@ -126,7 +124,7 @@ function openCore() {
 	}
 }
 
-function changeCoreView(eq: CoreEquipment) {
+function changeCoreView(eq: coreEquipment) {
 	temp.coreViewEquipment = eq;
 }
 </script>
@@ -255,7 +253,7 @@ function changeCoreView(eq: CoreEquipment) {
 			" :style="{'border-color': temp.coreViewColor()}">
 				<div v-if="temp.coreViewEquipment.rarity != undefined">
 					<span v-html="equipmentDisplay(temp.coreViewEquipment)" /><br>
-					真实等级{{equipmentAttribute(temp.coreViewEquipment).realLevel.toFixed(1)}}(稀有度加成{{((temp.coreViewEquipment.rarity ?? 0) ** 2 * 100).toFixed(1)}}%)<br>
+					真实等级{{equipmentAttribute(temp.coreViewEquipment).realLevel.toFixed(1)}}(稀有度加成{{(temp.coreViewEquipment.rarity ** 2 * 100).toFixed(1)}}%)<br>
 					生命值+{{equipmentAttribute(temp.coreViewEquipment).hea.toFixed(1)}}<br>
 					攻击力+{{equipmentAttribute(temp.coreViewEquipment).atk.toFixed(1)}}<br>
 					防御力+{{equipmentAttribute(temp.coreViewEquipment).def.toFixed(1)}}<br>
