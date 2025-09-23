5a7c272bf5395da05a4a82543dc6d6cfaa419b20
VeryrrDefine <1308061599@qq.com>
2025-09-23 20:17:02 +0800

    bhz

--- src/core/ordinal/well_ordering.ts
+++ src/core/ordinal/well_ordering.ts
@@ -5,5 +5,40 @@ import { format, formatWhole } from '@/utils/format';
 import { upgrades, buyables } from '../mechanic';
 import Decimal from 'break_eternity.js';
 import { player } from '../save';
+import type { FixedLengthArray } from 'type-fest';
+import { DC } from '../constants';
 
-export const WellOrderingBuyables = [] as const
+export const WellOrderingBuyables = [] as const;
+export const nt = {
+	get p() {
+		return player.numbertheory.well_ordering;
+	},
+};
+export function wellOrderGainPerClick() {
+	let a = new Decimal(1);
+
+	return a;
+}
+export function clickWellOrder() {
+	nt.p.energy = nt.p.energy.add(wellOrderGainPerClick());
+}
+export function stepProceed(x: number) {
+	if (!nt.p.steps_proceeded.includes(x)) {
+		if (x == 1 && nt.p.energy.gte(DC.D_10)) {
+			nt.p.energy = nt.p.energy.sub(DC.D_10);
+			nt.p.steps_proceeded.push(x);
+		}
+	}
+}
+export function wellOrderPlayerData() {
+	return {
+		selecting: 1,
+		energy: DC.D_0,
+		pages: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] as FixedLengthArray<number, 10>,
+		steps_proceeded: [] as number[],
+	};
+}
+
+export function energyToUNOCFSpeed() {
+	return player.numbertheory.well_ordering.energy.add(1).clampMin(1);
+}
