344e8591e3ed874ae18fa94e2942a8c010b64410
VeryrrDefine <1308061599@qq.com>
2025-10-01 18:13:30 +0800

    more musics

--- src/core/music.ts
+++ src/core/music.ts
@@ -5,8 +5,17 @@ let audioelement = document.getElementById('audio');
 
 export const MUSICS = [
 	'https://cdn.glitch.com/03a4b67b-6f18-4f6d-8d37-50a18fb615c8%2FGoing%20Down%20by%20Jake%20Chudnow%20%5BHD%5D.mp3?v=1581538237884',
+	'https://cdn.glitch.com/03a4b67b-6f18-4f6d-8d37-50a18fb615c8%2FHypnothis.mp3?v=1584285594822',
+	'https://cdn.glitch.com/310d7aca-4728-445f-9084-db26ceccd7b5%2FArseniy%20Shkljaev%20-%20Nuclearoids%20%5BTrance%5D.mp3?v=1591548196791',
+	'https://cdn.glitch.com/310d7aca-4728-445f-9084-db26ceccd7b5%2FHeaven%20and%20Hell%20-%20Jeremy%20Blake%20%5BMpgun.com%5D.mp3?v=1592859293921',
 ] as const;
-export const MUSIC_TEXT = ['None', 'Going Down by Jake Chudnow', '自定义'];
+export const MUSIC_TEXT = [
+	'None',
+	'Going Down by Jake Chudnow',
+	'Hypnothis',
+	'Arseniy Shkljaev- Nuclearoids (Trance)',
+	'Heaven and Hell - Jeremy Black',
+];
 export function checkAudioElementBefore(callback: (el: HTMLAudioElement) => void) {
 	if (!audioelement || !(audioelement instanceof HTMLAudioElement)) {
 		audioelement = document.getElementById('audio');
