9eb1d31f7609897848c9903b140b5cdc10cd9972
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-10-01 23:06:24 +0800

    Update Side.vue

--- src/components/group-2/Side.vue
+++ src/components/group-2/Side.vue
@@ -19,7 +19,7 @@ import { isTester } from '@/core/save/testing.ts';
 					>
 						大数之路重制版
 					</div>
-					<div style="font-size: 15px">UNOCF Beta</div>
+					<div style="font-size: 15px">Retribution Beta</div>
 				</div>
 			</div>
 		</div>
