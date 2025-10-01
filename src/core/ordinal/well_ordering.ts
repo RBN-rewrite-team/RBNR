5615777bdb0121da058597797aa5a62b15a20ce3
VeryrrDefine <1308061599@qq.com>
2025-10-01 19:17:22 +0800

    t

--- src/core/ordinal/well_ordering.ts
+++ src/core/ordinal/well_ordering.ts
@@ -211,7 +211,7 @@ export const WellOrderingUpgrades = {
 	})(),
 	U6R18: new (class extends Upgrade {
 		description = '推演能量×1e100';
-		cost = new Decimal('1e1790');
+		cost = new Decimal('1e1795');
 		name = 'U6-R-1-8';
 		currency: Currencies = Currencies.DEDUCE_ENERGY;
 		show(): boolean {
