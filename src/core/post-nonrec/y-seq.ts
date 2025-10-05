a7d6828b1627f56d914ad365aa6805db7a6376fe
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-10-05 12:37:42 +0800

    h

--- src/core/post-nonrec/y-seq.ts
+++ src/core/post-nonrec/y-seq.ts
@@ -53,7 +53,7 @@ export const Y_SEQ = {
 	  return new Decimal(2**384)
 	},
 	LogScalingRatio() {
-	  let base = new Decimal(1.05).log10()
+	  let base = new Decimal(1.15).log10()
 	  return base
 	},
 	buyDimensions(id: 0 | 1 | 2 | 3): void {
