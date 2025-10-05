8dce4da4721ae9354be6d10ff9a6921ad05d6984
VeryrrDefine <1308061599@qq.com>
2025-10-05 14:25:27 +0800

    window protect

--- src/core/automator/environment.ts
+++ src/core/automator/environment.ts
@@ -68,6 +68,7 @@ const minFunction = new (class MinFunction extends Callable {
 })();
 const getFunction = new (class GetFunction extends Callable {
 	async call(env: Environment, ...args: any[]) {
+		if (args[0] === window) throw new Error('检测到越界访问行为，该访问已被禁止');
 		return args[0][args[1]];
 	}
 })();
