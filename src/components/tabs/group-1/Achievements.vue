60f2a806232ce292b2e8e1b7715482a59c29266c
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-10-07 16:15:36 +0800

    h

--- src/components/tabs/group-1/Achievements.vue
+++ src/components/tabs/group-1/Achievements.vue
@@ -22,17 +22,16 @@ const numberdisplay = () => {
 			你有&nbsp;{{ player.achievements.length }}&nbsp;成就点
 		</div>
 	</div>
-	<div class="upgrade" style="height: 30px">
+	<div class="upgrade" style="min-height: 30px">
 		<div style="width: 250px; line-height: 30px">
 			成就点使<span v-html="numberdisplay()"></span>获取
 			<div style="display: inline; font-weight: bold; color: #169500">×1.000</div>
 			<br />
 			公式：
-			<br />
-			<vue-latex expression="(x^2)^{\sin^2x+\cos^2x-\frac{e^{2i\pi+x}}{\exp(x)}}"></vue-latex>
 		</div>
 	</div>
-	<div style="margin-top: 80px">
+			<vue-latex expression="\frac{\frac{d}{dx} \left[ \int_0^x \prod_{n=1}^{10} \left( \frac{\sin^2(nt) + \cos^2(nt)}{\sum_{k=1}^5 e^{\ln(\sec^2(kt) - \tan^2(kt))}} \right) dt \right]}{\frac{\partial}{\partial x} \left[ \int_0^x \prod_{m=1}^7 \left( \frac{\cosh^2(mu) - \sinh^2(mu)}{\sum_{p=1}^3 \left( \csc^2(pu) - \cot^2(pu) \right)} \right) du \right]} \times \frac{\sum_{j=1}^{1} \frac{j}{j}}{\prod_{i=1}^{1} \frac{i}{i}} \times \frac{\oint_{|z|=1} \frac{1}{z} dz}{\oint_{|z|=1} \frac{1}{z} dz}" display-mode></vue-latex>
+	<div>
 		<div class="upgrade" style="height: auto" v-for="row in Object.entries(achievements)">
 			<div
 				:class="
