1879d16a6c43577144712f779c20de2be4cc28fd
VeryrrDefine <1308061599@qq.com>
2025-10-08 21:55:01 +0800

    w

--- src/core/automator/a-objects.ts
+++ src/core/automator/a-objects.ts
@@ -63,7 +63,7 @@ export class Dictionary<K = any, V extends {} = any> {
 		let res = '(';
 		for (let a of this.mapEntries()) {
 			if (a[1] instanceof Dictionary) {
-				let q = '...';
+				let q = '{...}';
 				let pass = true;
 				if (parent) {
 					for (let i = 0; i < (parent.length ?? 0); i++) {
