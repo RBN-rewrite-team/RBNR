84a910a8f73746311c0a267fae7c7b50861adca6
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-10-16 18:55:52 +0800

    Update well_ordering.ts

--- src/core/ordinal/well_ordering.ts
+++ src/core/ordinal/well_ordering.ts
@@ -258,8 +258,8 @@ export const WellOrderingUpgrades = {
 	})(),
 	U6R18: new (class extends Upgrade {
 		description = () =>
-			`推演能量×1e100<br>解锁<b class="baseRetribution">${RETRIBUTION.name()}</b><br>困难模式下价格降低！`;
-		cost = () => new Decimal(player.options.hardMode ? '1e1788' : '1e1800');
+			`推演能量×1e100<br>解锁<b class="baseRetribution">${RETRIBUTION.name()}</b>`;
+		cost = () => new Decimal('1e1788');
 		name = 'U6-R-1-8';
 		currency: Currencies = Currencies.DEDUCE_ENERGY;
 		show(): boolean {
