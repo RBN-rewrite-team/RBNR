1b3752ba303a2546f31f6f6227956b7b8f1adcb5
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-10-17 21:59:49 +0800

    Update well_ordering.ts

--- src/core/ordinal/well_ordering.ts
+++ src/core/ordinal/well_ordering.ts
@@ -259,7 +259,7 @@ export const WellOrderingUpgrades = {
 	U6R18: new (class extends Upgrade {
 		description = () =>
 			`推演能量×1e100<br>解锁<b class="baseRetribution">${RETRIBUTION.name()}</b>`;
-		cost = () => new Decimal('1.794e1794');
+		cost = () => new Decimal('1e1777');
 		name = 'U6-R-1-8';
 		currency: Currencies = Currencies.DEDUCE_ENERGY;
 		show(): boolean {
