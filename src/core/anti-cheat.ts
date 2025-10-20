aab93e532fe184415240e3b289d4783eb9f23ae5
VeryrrDefine <1308061599@qq.com>
2025-10-20 21:43:21 +0800

    feat. remove detect Timer Hooker

--- src/core/anti-cheat.ts
+++ src/core/anti-cheat.ts
@@ -54,21 +54,21 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 let cheat = false;
 
-function detectTimerHooker(): void {
-	if (document.getElementsByClassName('_th-container')[0] != void 0) {
-		if (cheat) throw new Error('?'); // 防止卡死
-		clearInterval(loopInterval);
-		ModalService.show({
-			title: '作弊检测',
-			content: '检测到您正在使用作弊程序，请关闭后再试！',
-			closeOnClickMask: false,
-			showCancelButton: false,
-			showConfirmButton: false,
-		});
-		cheat = true;
-	}
-}
+// function detectTimerHooker(): void {
+// 	if (document.getElementsByClassName('_th-container')[0] != void 0) {
+// 		if (cheat) throw new Error('?'); // 防止卡死
+// 		clearInterval(loopInterval);
+// 		ModalService.show({
+// 			title: '作弊检测',
+// 			content: '检测到您正在使用作弊程序，请关闭后再试！',
+// 			closeOnClickMask: false,
+// 			showCancelButton: false,
+// 			showConfirmButton: false,
+// 		});
+// 		cheat = true;
+// 	}
+// }
 
-setInterval(detectTimerHooker, 1000);
+// setInterval(detectTimerHooker, 1000);
 
 export {};
