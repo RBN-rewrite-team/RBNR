c82fc03bf33226b1aaae3abddcc4d252387365ca
VeryrrDefine <1308061599@qq.com>
2025-10-21 20:56:27 +0800

    q

--- src/components/tabs/pt/Analysis.vue
+++ src/components/tabs/pt/Analysis.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { player, feature } from '@/core/global.ts';
-import { Analysis, dayOfWeek, realPTreset } from '@/core/pt/index.ts';
+import { Analysis, dayOfWeek, PTEffects, realPTreset } from '@/core/pt/index.ts';
+import { format } from '@/utils/format';
 import type { IntClosedRange } from 'type-fest';
 
 function unlockedList(): string {
@@ -18,8 +19,22 @@ function unlockedList(): string {
 		进行证明论重置以随机解析，成功率为 5% ，解析同一系统20次必定成功。<br />
 		单一系统首次解析必定成功。<br />
 	</h3>
+	<div style="border: 1px solid cyan; width: 50%; margin: auto">
+		<p>证明论重置次数带来以下奖励:</p>
+		<p>基数以前全局速度*{{ format(PTEffects.effectToPreCardinal()) }}，上限*5</p>
+		<p>
+			九头蛇对数软上限减弱{{ format(PTEffects.effectToHydraEnergyLogSoftCap()) }}%，上限-50%
+		</p>
+		<p>溶液获取速度*{{ format(PTEffects.effectToSolutions()) }}，上限*4</p>
+		<p>
+			每次非递归重置，非递归获取次数*{{
+				format(PTEffects.effectToNonrecResetTimes())
+			}}，上限*25
+		</p>
+		<p>NRC目标等级减小{{ format(PTEffects.effectToNonrecChallengeGoalLevel()) }}%，上限-50%</p>
+	</div>
 	<!--重置先放这里，反正也不常重置-->
-	<button class="clickable_button" @click="realPTreset">证明论重置</button>
+	<button class="clickable_button" @click="realPTreset" style="margin: auto">证明论重置</button>
 	<div
 		v-for="count in 7"
 		align="center"
