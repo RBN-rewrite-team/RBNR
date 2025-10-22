4bec80e13681cfea444206b043a1fefcdee1cc04
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-10-22 21:22:39 +0800

    Update Analysis.vue

--- src/components/tabs/pt/Analysis.vue
+++ src/components/tabs/pt/Analysis.vue
@@ -13,13 +13,13 @@ function unlockedList(): string {
 
 <template>
 	<br />
-	<h3 style="color: cyan">
+	<h3 class="pt_base" style="color: cyan; width: 50%; margin: auto">
 		解析系统中，每周的不同时段会解锁不同系统。<br />
 		当前是 {{ dayOfWeek()[1] }} ，解锁 <span v-html="unlockedList()" />系统。<br />
 		进行证明论重置以随机解析，成功率为 5% ，解析同一系统20次必定成功。<br />
 		单一系统首次解析必定成功。<br />
 	</h3>
-	<div style="border: 1px solid cyan; width: 50%; margin: auto">
+	<div class="pt_base" style="width: 50%; margin: auto">
 		<p>证明论重置次数带来以下奖励:</p>
 		<p>基数以前全局速度*{{ format(PTEffects.effectToPreCardinal()) }}，上限*5</p>
 		<p>
@@ -34,14 +34,14 @@ function unlockedList(): string {
 		<p>NRC目标等级减小{{ format(PTEffects.effectToNonrecChallengeGoalLevel()) }}%，上限-50%</p>
 	</div>
 	<!--重置先放这里，反正也不常重置-->
-	<button class="clickable_button" @click="realPTreset" style="margin: auto">证明论重置</button>
+	<div class="pt_base pt_reset" @click="realPTreset" style="width: 50%; margin: auto"><span class="pt_font">证明论重置</span></div>
 	<div
 		v-for="count in 7"
 		align="center"
 		:style="{ opacity: Analysis.analysisUnlocked(count - 1) ? 1 : 0.5 }"
 	>
 		<br />
-		<div class="system">
+		<div class="pt_base system">
 			<h3 v-html="count + ': ' + Analysis.systems[count - 1]" />
 			<br />
 			解析进度：{{ player.pt.analysis[count - 1] }}/11(本次解析已尝试{{
@@ -57,10 +57,27 @@ function unlockedList(): string {
 </template>
 
 <style scoped>
+.pt_base {
+	background-color: rgba(0, 255, 255, 0.5);
+	border: 2px solid rgba(0, 255, 255, 0.75);
+}
 .system {
 	height: 200px;
 	width: calc(100% - 20px);
 	border: 2px solid cyan;
 	color: cyan;
+}
+.pt_reset {
+	height: 100px;
+	color: cyan;
+	font-weight: bold;
+	font-size: 16px;
+	position: relative;
+}
+.pt_font {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 }
 </style>
