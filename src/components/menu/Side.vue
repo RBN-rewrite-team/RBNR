cec9080786caf3a559bd2d84b8dc0ec0c709d034
VeryrrDefine <1308061599@qq.com>
2025-10-25 10:00:24 +0800

    improve. 分化菜单子选项

--- src/components/menu/Side.vue
+++ src/components/menu/Side.vue
@@ -4,6 +4,7 @@ import { Logarithm } from '../../core/exponention/logarithm';
 import { unlockedPlots, viewedPlotLength } from '@/core/plot';
 import { isTester } from '@/core/save/testing';
 import { RETRIBUTION } from '@/core/post-nonrec/retribution';
+import SubMenuObject from './SubMenuObject.vue';
 </script>
 
 <template>
@@ -27,127 +28,61 @@ import { RETRIBUTION } from '@/core/post-nonrec/retribution';
 		<div class="menu" id="menu">
 			<div class="background" style="overflow: auto">
 				<div class="main">
-					<div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 1225 }"
-							@click="player.currentTab = 1225"
-						>
-							地下城
-						</div>
-					</div>
+					<SubMenuObject :tab="1225" text="地下城" />
 					<div
 						v-if="
 							player.timeshard.unlAuto && (player.firstResetBit & 0b10000) == 0b10000
 						"
 					>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 167 }"
-							@click="player.currentTab = 167"
-						>
-							自动机
-						</div>
+						<SubMenuObject :tab="167" text="自动机" />
 					</div>
 					<div v-if="player.singularity.stage < 11">
 						<div class="menu1">后继</div>
 						<div class="menu_line"></div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 0 }"
-							@click="player.currentTab = 0"
-						>
-							后继
-						</div>
+						<SubMenuObject :tab="0" text="后继" />
 					</div>
 					<template v-if="player.upgrades[13] && player.singularity.stage < 10">
 						<div class="menu1">加法</div>
 						<div class="menu_line"></div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 2 }"
-							@click="player.currentTab = 2"
-						>
-							加法
-						</div>
+						<SubMenuObject :tab="2" text="加法" />
 					</template>
 					<template v-if="player.upgrades[26] && player.singularity.stage < 9">
 						<div class="menu1">乘法</div>
 						<div class="menu_line"></div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 4 }"
-							@click="player.currentTab = 4"
-						>
-							乘法
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 5 }"
-							@click="player.currentTab = 5"
+						<SubMenuObject :tab="4" text="乘法" />
+						<SubMenuObject
+							:tab="5"
+							text="质因数"
 							v-if="player.firstResetBit & 0b10 && player.singularity.stage < 7"
-						>
-							质因数
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 6 }"
-							@click="player.currentTab = 6"
+						/>
+
+						<SubMenuObject
+							:tab="6"
+							text="数论研究"
 							v-if="player.upgrades[35] && player.singularity.stage < 8"
-						>
-							数论研究
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 8 }"
-							@click="player.currentTab = 8"
+						/>
+						<SubMenuObject
+							:tab="8"
+							text="乘法挑战"
 							v-if="player.singularity.stage < 6 && player.upgrades[39]"
-						>
-							乘法挑战
-						</div>
+						/>
 					</template>
 					<template v-if="player.firstResetBit & 0b100 && player.singularity.stage < 9">
 						<div class="menu1">指数</div>
 						<div class="menu_line"></div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 9 }"
-							@click="player.currentTab = 9"
-						>
-							指数升级
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 10 }"
-							@click="player.currentTab = 10"
-							v-if="player.upgrades[47]"
-						>
-							棋盘
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 12 }"
-							@click="player.currentTab = 12"
-							v-if="player.milestones.cb5"
-						>
-							对数运算
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 13 }"
-							@click="player.currentTab = 13"
+						<SubMenuObject :tab="9" text="指数升级" />
+						<SubMenuObject :tab="10" text="棋盘" v-if="player.upgrades[47]" />
+						<SubMenuObject :tab="12" text="对数运算" v-if="player.milestones.cb5" />
+						<SubMenuObject
+							:tab="13"
+							text="对数膨胀"
 							v-if="player.singularity.stage < 1 && player.milestones.log_G"
-						>
-							对数膨胀
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 14 }"
-							@click="player.currentTab = 14"
+						/>
+						<SubMenuObject
+							:tab="14"
+							text="奇点生成器"
 							v-if="player.singularity.enabled || player.milestones.dil_7"
-						>
-							奇点生成器
-						</div>
+						/>
 					</template>
 					<template v-if="player.singularity.stage >= 4">
 						<div class="menu1">
@@ -156,169 +91,65 @@ import { RETRIBUTION } from '@/core/post-nonrec/retribution';
 							<span v-else>序数</span>
 						</div>
 						<div class="menu_line"></div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 14 }"
-							@click="player.currentTab = 14"
-						>
-							奇点生成器
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 15 }"
-							@click="player.currentTab = 15"
+						<SubMenuObject :tab="14" text="奇点生成器" />
+						<SubMenuObject
+							:tab="15"
+							text="序数"
 							v-if="player.firstResetBit & 0b1000 && player.retribution == 0"
-						>
-							序数
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 6 }"
-							@click="player.currentTab = 6"
-							v-if="player.upgrades[58]"
-						>
-							数论研究
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 17 }"
-							@click="player.currentTab = 17"
+						/>
+						<SubMenuObject :tab="6" text="数论研究" v-if="player.upgrades[58]" />
+						<SubMenuObject
+							:tab="17"
+							text="加速器"
 							v-if="player.upgrades[59] && player.retribution == 0"
-						>
-							加速器
-						</div>
+						/>
 					</template>
 					<template v-if="player.upgrades[517]">
 						<div class="menu1">九头蛇</div>
 						<div class="menu_line"></div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 19 }"
-							@click="player.currentTab = 19"
-							v-if="player.upgrades[517]"
-						>
-							九头蛇引擎
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 27 }"
-							@click="player.currentTab = 27"
-							v-if="player.retribution >= 1"
-						>
-							Y序列
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 20 }"
-							@click="player.currentTab = 20"
-							v-if="player.upgrades['69R']"
-						>
-							稀释
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 28 }"
-							@click="player.currentTab = 28"
+						<SubMenuObject :tab="19" text="九头蛇引擎" />
+						<SubMenuObject :tab="27" text="Y序列" v-if="player.retribution >= 1" />
+						<SubMenuObject :tab="20" text="稀释" v-if="player.upgrades['69R']" />
+						<SubMenuObject
+							:tab="28"
+							:text="RETRIBUTION.name()"
 							v-if="player.upgrades['U6R18']"
-						>
-							{{ RETRIBUTION.name() }}
-						</div>
+						/>
 					</template>
 					<template v-if="(player.firstResetBit & 0b10000) == 0b10000">
 						<div class="menu1">非递归</div>
 						<div class="menu_line"></div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 21 }"
-							@click="player.currentTab = 21"
-							v-if="(player.firstResetBit & 0b10000) == 0b10000"
-						>
-							非递归里程碑
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 23 }"
-							@click="player.currentTab = 23"
-							v-if="(player.firstResetBit & 0b10000) == 0b10000"
-						>
-							能量因素
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 24 }"
-							@click="player.currentTab = 24"
-							v-if="(player.firstResetBit & 0b10000) == 0b10000"
-						>
-							非递归研究树
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 22 }"
-							@click="player.currentTab = 22"
-							v-if="(player.firstResetBit & 0b10000) == 0b10000"
-						>
-							非递归挑战
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 25 }"
-							@click="player.currentTab = 25"
-							v-if="
-								(player.firstResetBit & 0b10000) == 0b10000 &&
-								player.milestones.nonrec_18
-							"
-						>
-							非递归升级
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 26 }"
-							@click="player.currentTab = 26"
+						<SubMenuObject :tab="21" text="非递归里程碑" />
+						<SubMenuObject :tab="23" text="能量因素" />
+						<SubMenuObject :tab="24" text="非递归研究树" />
+						<SubMenuObject :tab="22" text="非递归挑战" />
+						<SubMenuObject
+							:tab="25"
+							text="非递归升级"
+							v-if="player.milestones.nonrec_18"
+						/>
+						<SubMenuObject
+							:tab="26"
+							text="UNOCF"
 							v-if="
 								(player.firstResetBit & 0b10000) == 0b10000 &&
 								player.milestones.nonrec_19
 							"
-						>
-							UNOCF
-						</div>
+						/>
 					</template>
 					<template
 						v-if="player.retribution >= 1 && player.nonrecu.studies_bought.includes(30)"
 					>
 						<div class="menu1">证明论</div>
 						<div class="menu_line"></div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 29 }"
-							@click="player.currentTab = 29"
-						>
-							解析
-						</div>
-						<div
-							class="menu2"
-							:class="{ focus: player.currentTab == 30 }"
-							@click="player.currentTab = 30"
-						>
-							解析里程碑
-						</div>
+						<SubMenuObject :tab="29" text="解析" />
+						<SubMenuObject :tab="30" text="解析里程碑" />
 					</template>
 
 					<div class="menu1">杂项</div>
 					<div class="menu_line"></div>
-					<div
-						class="menu2"
-						:class="{ focus: player.currentTab == 1 }"
-						@click="player.currentTab = 1"
-					>
-						设置
-					</div>
-					<div
-						class="menu2"
-						:class="{ focus: player.currentTab == 3 }"
-						@click="player.currentTab = 3"
-					>
-						关于游戏
-					</div>
+					<SubMenuObject :tab="1" text="设置" />
+					<SubMenuObject :tab="3" text="关于游戏" />
 					<div
 						class="menu2"
 						:class="{ focus: player.currentTab == 200 }"
@@ -330,35 +161,11 @@ import { RETRIBUTION } from '@/core/post-nonrec/retribution';
 							>+{{ unlockedPlots() - viewedPlotLength() }}</span
 						>
 					</div>
-					<div
-						class="menu2"
-						:class="{ focus: player.currentTab == 7 }"
-						@click="player.currentTab = 7"
-					>
-						统计
-					</div>
-					<div
-						class="menu2"
-						:class="{ focus: player.currentTab == 11 }"
-						@click="player.currentTab = 11"
-					>
-						成就
-					</div>
-					<div
-						class="menu2"
-						:class="{ focus: player.currentTab == 16 }"
-						@click="player.currentTab = 16"
-						v-if="player.firstResetBit & 0b1000"
-					>
-						帮助
-					</div>
-					<div
-						class="menu2"
-						:class="{ focus: player.currentTab == 18 }"
-						@click="player.currentTab = 18"
-					>
-						时间碎片
-					</div>
+					<SubMenuObject :tab="7" text="统计" />
+					<SubMenuObject :tab="11" text="成就" />
+
+					<SubMenuObject :tab="16" text="帮助" v-if="player.firstResetBit & 0b1000" />
+					<SubMenuObject :tab="18" text="时间碎片" />
 				</div>
 			</div>
 		</div>
