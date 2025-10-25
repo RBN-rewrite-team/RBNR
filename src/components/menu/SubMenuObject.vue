25bd8b35a8e7b27e9c8b00829c54100ddaa73c40
VeryrrDefine <1308061599@qq.com>
2025-10-25 10:00:24 +0800

    improve. 分化菜单子选项

---
+++ src/components/menu/SubMenuObject.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { player } from '@/core/save';
+import Baixie from '../group-2/Baixie.vue';
+
+const props = defineProps<{
+	tab: number;
+	text: string;
+}>();
+</script>
+
+<template>
+	<div>
+		<div
+			class="menu2"
+			:class="{ focus: player.currentTab == tab }"
+			@click="player.currentTab = tab"
+		>
+			{{ text }}<Baixie />
+		</div>
+	</div>
+</template>
