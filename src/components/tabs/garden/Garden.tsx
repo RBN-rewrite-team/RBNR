6b25282b9353cb9603175b1cb35765c9e5afbefd
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-10-26 19:19:28 +0800

    Update Garden.tsx

--- src/components/tabs/garden/Garden.tsx
+++ src/components/tabs/garden/Garden.tsx
@@ -66,6 +66,7 @@ function simulateText(canvasRef: any) {
 							player.garden.focusNode = g;
 						}}
 					>
+						//@ts-ignore
 						<h2 style="position: relative; bottom: -80px">{g.name}</h2><h3 style="position: absolute; top: -30px">x{formatWhole(player.garden.generators[g.key])}</h3>
 						<br />
 						<span style="position: relative; bottom: -60px">
