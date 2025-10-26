bf0295df17fa9ea6899028d1e82ee482cec4c4eb
VeryrrDefine <1308061599@qq.com>
2025-10-26 18:31:16 +0800

    fix. 清理了一些无用的注释

--- src/components/tabs/garden/Garden.tsx
+++ src/components/tabs/garden/Garden.tsx
@@ -121,12 +121,6 @@ export default defineComponent({
 					ref={canvasRef}
 				>
 					<div class={'canvas_corner'}>
-						{/* <div
-							class="node"
-							style={{ ...getNodeStyle(0, 0, canvasRef), borderColor: 'white' }}
-						>
-							<span class="node_desc">{JSON.stringify(temp.garden)}</span>
-						</div> */}
 						{player.garden.openSimulate ? (
 							simulateText(canvasRef)
 						) : (
@@ -143,49 +137,6 @@ export default defineComponent({
 								</GardenNode>
 							</>
 						)}
-						{/*<GardenNode x={-100} y={-100} canvasRef={canvasRef}>
-							百因必有果，你的报应就是我
-						</GardenNode>
-						<GardenNode x={300} y={0} canvasRef={canvasRef}>
-							还没做完
-						</GardenNode>
-						<GardenNode x={0} y={0} canvasRef={canvasRef}>
-							你有0 ω病毒
-						</GardenNode>
-						<GardenNode
-							x={100}
-							y={100}
-							canvasRef={canvasRef}
-							onClick={function () {
-								ModalService.show({
-									title: '拜谢',
-									content: '拜谢',
-								});
-							}}
-						>
-							那我问你
-							<Baixie />
-						</GardenNode>*/}
-						{/* 
-                        <div 
-                            class="node_conn" 
-                            style="border-color: white; transform: translate(-50%, -50%) rotate(90deg); top: 96px; left: 627px; width: 1000px"
-                        >
-                            <span class="node_desc">Boost</span></div>
-                        <div  
-                            class="node_conn" 
-                            style="border-color: white; transform: translate(-50%, -50%) rotate(116.56505117707799deg); top: 696px; left: 577px; width: 223.60679774997897px"
-                        ></div>
-                        <div 
-                            class="node" 
-                            style="transform: translate(-50%, -50%); top: 796px; left: 527px; border-color: white" 
-                        >
-                            <span class="node_desc">何意味</span>
-                        </div>
-                        <div 
-                            class="node" 
-                            style="transform: translate(-50%, -50%); top: 996px; left: 827px; border-color: white"
-                        ><span class="node_desc">百因必有果，你的报应就是我</span></div> */}
 					</div>
 				</div>
 			</>
