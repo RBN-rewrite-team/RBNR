389df6e9349a2f0413fb67db2325dd7f8047331a
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-10-26 11:09:05 +0800

    h

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -39,6 +39,7 @@ import DeveloperMode from './tabs/devonly/DeveloperMode.vue';
 import Analysis from './tabs/pt/Analysis.vue';
 import AnalysisMilestones from './tabs/pt/AnalysisMilestones.vue';
 import SingularityGenerator from './tabs/group-1/SingularityGenerator.vue';
+import Garden from './components/tabs/garden/Garden.tsx';
 
 const tabComponents = {
   0: Successor,
@@ -72,6 +73,7 @@ const tabComponents = {
   28: Retribution,
   29: Analysis,
   30: AnalysisMilestones,
+  31: Garden,
   167: AutomatorEditor,
   200: PlotTab,
   300: SaveBank,
