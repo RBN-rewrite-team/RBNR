8c717c30947ea22b099cd63264c54e50219e1b05
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-10-26 22:13:41 +0800

    Update Resources.vue

--- src/components/group-2/Resources.vue
+++ src/components/group-2/Resources.vue
@@ -30,7 +30,7 @@ import { isDeveloper } from '@/core/save/testing.ts';
 						</div>
 						<br />
 						<div style="font-size: 14px; color: purple">
-							子世界发电机和升级价格x{{ format(Garden.entropyEffect()) }}
+							子世界发电机和升级价格x{{ format(Garden.entropyEffect()) }}(没做完)
 						</div>
 					</div>
 					<div class="resource" style="margin-left: 685px">
