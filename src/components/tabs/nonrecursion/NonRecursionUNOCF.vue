ba4f5b0eeb15fecd9543979119700aae2030f51c
VeryrrDefine <1308061599@qq.com>
2025-11-01 09:34:14 +0800

    w

--- src/components/tabs/nonrecursion/NonRecursionUNOCF.vue
+++ src/components/tabs/nonrecursion/NonRecursionUNOCF.vue
@@ -13,11 +13,7 @@ const cur = computed(() =>
 
 <template>
 	<div class="main">
-		<p style="color: grey; table-align: center">
-			<vue-latex
-				expression="\Pi_2 \operatorname{onto}\mathrm{Ord}\subset \{a|\operatorname{cf}(a)=a\}"
-			/>。
-		</p>
+		<p style="color: grey; table-align: center"><Baixie />。</p>
 
 		<p>
 			你的UNOCF序数为<vue-latex
