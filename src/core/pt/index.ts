7e4b991942b46c68d38f6dbdf666ecf123b89539
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-11-06 13:21:02 +0800

    Update index.ts

--- src/core/pt/index.ts
+++ src/core/pt/index.ts
@@ -316,6 +316,7 @@ export type GardenGenerator = {
 	unlocked(): boolean;
 	connect: [number[], number[]];
 	show?(): boolean;
+	igNR?(): boolean;
 	effectDescription?(): string;
 };
 
@@ -344,6 +345,9 @@ export function isGardenUpgrade(x: GardenUpgrade | GardenGenerator): x is Garden
 }
 export function isGardenGenerator(x: GardenUpgrade | GardenGenerator): x is GardenGenerator {
 	return x.isG;
+}
+export function igGNR(x: GardenGenerator) {
+	return x.igNR?.() ?? false;
 }
 export function ignoreNR(x: GardenUpgrade) {
 	return x.igNR?.() ?? false;
@@ -490,6 +494,7 @@ export const GardenGenUpgs = {
 			show: (): boolean => Garden.boughtUpgrade(23),
 			connect: [[], []],
 			effectDescription: (x: Decimal): string => '想法产量×' + format(x.mul(0.01).add(1)),
+			igNR: (): boolean => true,
 		},
 		8: {
 			isG: true,
@@ -504,6 +509,7 @@ export const GardenGenUpgs = {
 			show: (): boolean => Garden.boughtUpgrade(23),
 			connect: [[7], []],
 			effectDescription: (x: Decimal): string => '想法产量×' + format(x.mul(0.02).add(1)),
+			igNR: (): boolean => true,
 		},
 	} satisfies {
 		[key in any]: GardenGenerator;
@@ -1328,9 +1334,11 @@ export const Garden = {
 		player.garden.entropy = new Decimal(0);
 		player.garden.totalEntropy = new Decimal(0);
 		player.garden.bestEntropy = new Decimal(0);
-		for (const i in player.garden.generators)
-			player.garden.generators[Number(i) as keyof typeof GardenGenUpgs.generators] =
-				new Decimal(0);
+		for (const i in player.garden.generators) {
+			if(!igGNR(GardenGenUpgs.generators[Number(i) as keyof typeof GardenGenUpgs.generators]))
+				player.garden.generators[Number(i) as keyof typeof GardenGenUpgs.generators] =
+					new Decimal(0);
+		}
 		for (const i in player.garden.upgrades) {
 			if (!ignoreNR(GardenGenUpgs.upgrades[Number(i) as keyof typeof GardenGenUpgs.upgrades]))
 				player.garden.upgrades[i] = false;
