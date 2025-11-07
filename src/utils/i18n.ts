1c1b0a2868e955730e5253fd11e12ca841327ab8
VeryrrDefine <1308061599@qq.com>
2025-11-07 22:55:41 +0800

    as const

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,19 +1,21 @@
-import { createI18n } from 'vue-i18n';
+import { createI18n, useI18n } from 'vue-i18n';
 
 import zhCN from '@/locales/zh_CN.tsx';
 import enUS from '@/locales/en_US.tsx';
 export const messages = {
 	'zh-CN': zhCN,
 	'en-US': enUS,
-};
+} as const;
 export const i18n = createI18n({
 	locale: 'en-US',
 	fallbackLocale: 'zh-CN',
 	messages,
-});
-
+} as const);
 export const messagesLength = (function () {
-	const a = {} as { [key in keyof typeof messages]: number };
+	const a = {
+		'zh-CN': 0,
+		'en-US': 0,
+	};
 	for (const key in messages) {
 		a[key as keyof typeof messages] = Object.keys(
 			messages[key as keyof typeof messages],
