c71f206c3ab7427da9439af6b28430615575261b
VeryrrDefine <1308061599@qq.com>
2025-11-12 19:45:07 +0800

    bq

--- src/core/news-data-en-US.ts
+++ src/core/news-data-en-US.ts
@@ -14,10 +14,11 @@ const news = [
 {id: "newsen10", text: "LHO=(ω→ω→LRO)_2"},
 {id: "newsen11", text: "Update after 5^5 hours"},
 {id: "newsen12", text: "Darkest night, I'll confront you here"},
-{id: "newsen13", text: "“P¤1-t2~u~ér.2-_/io~/5é &-cnh-”"},
+{id: "newsen13", text: "P¤1-t2~u~ér.2-_/io~/5é &-cnh-"},
 {id: "newsen14", text: "Don't you already have two of these?"},
 {id: 'newsen15', text: "37 users in this group, they'll become 37 corpse tomorrow...No, 3.7×10<sup>9</sup>."},
 {id: "newsen16", text: "<span style=\"color: rgb(0, 225, 255)\">After <b>F1.7977e308</b> of mass gain will softcap^8 mass gain!</span>"},
+{id: "newsen17", text:"你好"}
 ] as const satisfies {
     id: string;
     text: string;
