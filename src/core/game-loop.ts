aa5fa3686f547c336a938bf2fd8541d59ab3a596
VeryrrDefine <1308061599@qq.com>
2025-11-20 20:28:03 +0800

    Heyuweu

--- src/core/game-loop.ts
+++ src/core/game-loop.ts
@@ -149,10 +149,10 @@ export function gameLoop() {
 		for (let i = 1; i <= unlp; i++) {
 			if (!player.checkedPlots.includes(i) && temp.plotdisplay == 0) enterPlot(i);
 		}
-		if (player.milestones.dut_10) {
-			player.hydra.dilute.solutionCost = new Decimal(0);
-		}
 	};
+	if (player.milestones.dut_10) {
+		player.hydra.dilute.solutionCost = new Decimal(0);
+	}
 	equipmentDestroyLoop();
 	achLoop();
 	document.title = getCurTitle();
