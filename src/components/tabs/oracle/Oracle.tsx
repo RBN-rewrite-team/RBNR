b40bf1ea1b88924500f27ce7ac4acafb60031fdb
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-11-23 19:08:13 +0800

    Update Oracle.tsx

--- src/components/tabs/oracle/Oracle.tsx
+++ src/components/tabs/oracle/Oracle.tsx
@@ -1,15 +1,24 @@
 import Baixie from '@/components/group-2/Baixie.vue';
+import { Oracle } from '@/core/pt/oracle/oracle.ts';
+import { player } from '@/core/save/index.ts';
 import { defineComponent } from 'vue';
+import { format, formatWhole } from '@/utils/format.ts';
 
 export default defineComponent({
 	name: 'Oracle',
 	setup(props, ctx) {
 		return () => (
 			<>
-				<p>
-					没做完
-					<Baixie />
-				</p>
+				<div align="center">
+					<br/>
+					<div style="width: calc(100% - 40px); height: 200px; padding: 5px; border: 1px solid red; color: red">
+						<h2>神谕进程</h2><br/>
+						神谕比特<h3>{formatWhole(Oracle.nowBitsHave())}/{formatWhole(player.oracle.totalBits)}</h3>
+						<div style="width: 100%; height: 40px; border: 1px solid orange" align="left">
+							<div style={{width: Oracle.bitGainProgress() * 100 + '%', height: '100%', 'background-color': 'red'}}></div>
+						</div>
+					</div>
+				</div>
 			</>
 		);
 	},
