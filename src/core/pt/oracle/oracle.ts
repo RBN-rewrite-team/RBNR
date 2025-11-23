b2f0db63df9e0264889686d2e0a685257abd6072
VeryrrDefine <1308061599@qq.com>
2025-11-23 11:45:48 +0800

    bx

--- src/core/pt/oracle/oracle.ts
+++ src/core/pt/oracle/oracle.ts
@@ -1,11 +1,11 @@
 import { player } from '@/core/global';
 import { format } from '@/utils/format';
 import Decimal, { type DecimalSource } from 'break_eternity.js';
-import { DC } from '../constants';
+import { DC } from '../../constants';
 import ModalService from '@/utils/Modal';
 import { predictableRandom } from '@/utils/algorithm.ts';
-import { deepCopy } from '../save';
-import { updateResetStatData } from '../stats';
+import { deepCopy } from '../../save';
+import { updateResetStatData } from '../../stats';
 import { getMessage, i18n } from '@/utils/i18n';
 import type { $t } from '@/utils/types';
 import { Garden } from '../garden.ts';
