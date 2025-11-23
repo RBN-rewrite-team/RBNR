bd013d23aed89c975a48952769e0f21cc0d6fbe9
VeryrrDefine <1308061599@qq.com>
2025-11-23 19:46:16 +0800

    Fix Type error and style

--- src/components/tabs/oracle/Oracle.tsx
+++ src/components/tabs/oracle/Oracle.tsx
@@ -9,13 +9,26 @@ export default defineComponent({
 	setup(props, ctx) {
 		return () => (
 			<>
-				<div align="center">
-					<br/>
-					<div style="width: calc(100% - 40px); height: 200px; padding: 5px; border: 1px solid red; color: red">
-						<h2>神谕进程</h2><br/>
-						神谕比特<h3>{formatWhole(Oracle.nowBitsHave())}/{formatWhole(player.oracle.totalBits)}</h3>
-						<div style="width: 100%; height: 40px; border: 1px solid orange" align="left">
-							<div style={{width: Oracle.bitGainProgress() * 100 + '%', height: '100%', 'background-color': 'red'}}></div>
+				<div style={{textAlign: "center", margin: "auto"}}>
+					<br />
+					<div style="width: calc(100% - 80px); height: 200px; padding: 5px; border: 1px solid red; color: red; margin: auto;">
+						<h2>神谕进程</h2>
+						<br />
+						神谕比特
+						<h3>
+							{formatWhole(Oracle.nowBitsHave())}/
+							{formatWhole(player.oracle.totalBits)}
+						</h3>
+						<div
+							style="width: 100%; height: 40px; border: 1px solid orange; text-align: left"
+						>
+							<div
+								style={{
+									width: Oracle.bitGainProgress() * 100 + '%',
+									height: '100%',
+									'background-color': 'red',
+								}}
+							></div>
 						</div>
 					</div>
 				</div>
