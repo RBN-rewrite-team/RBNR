e510515c35d9c5d7d5b207c2a670d28e83315128
VeryrrDefine <1308061599@qq.com>
2025-11-30 11:09:47 +0800

    w

--- src/core/nonrecu/nonrec-bms/index.ts
+++ src/core/nonrecu/nonrec-bms/index.ts
@@ -23,7 +23,7 @@ export const NON_REC_BMS = {
 		let base: [Decimal] = [new Decimal(1)];
 		base[0] = player.pt.nonrecBMS.deduce.clampMin(1).log10();
 		if (base[0].gte(1.55)) {
-			base[0] = base[0].div(1.55).pow(0.1).mul(1.55);
+			base[0] = base[0].div(1.55).pow(0.3).mul(1.55);
 		}
 		return base;
 	},
