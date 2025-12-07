142a8e54df5fa038f63e7ac02ccdc81b0bfff5d9
VeryrrDefine <1308061599@qq.com>
2025-12-07 12:59:54 +0800

    update news

--- src/core/news-data-en-US.ts
+++ src/core/news-data-en-US.ts
@@ -14,7 +14,36 @@ const news = [
 {id: "newsen11", text: "Update after 5^5 hours"},
 {id: "newsen14", text: "Don't you already have two of these?"},
 {id: "newsen16", text: "<span style=\"color: rgb(0, 225, 255)\">After <b>F1.7977e308</b> of mass gain will softcap^8 mass gain!</span>"},
-
+{
+    id: "a131",
+    text:
+      `Warning - We have just been informed that there is a chance of infection with a mind-virus of the Basilisk
+      type, similar to the infamous winking parrot. This particular example is known as 'Fractal Disease Type III'.
+      This is believed to cause a 'crashing' of the mind, similar to a computer crash, due to the mathematical
+      complexity of the image causing mathematical ideas that the mind can't comprehend, a Gondelian shock input
+      eventually leading to crashing through Gondelian spoilers. All who have researched it have eventually died
+      the same way, so it is impossible to tell exactly, but this is the common belief. Regardless, with the
+      introduction of 'design' mode, as well as reports of it's spontaneous appearance, sufficient repetition
+      of this mode's appearance may lead to an image forming in the mind similar to 'Fractal Disease Type III'.
+      With this in mind, we have some suggestions if you find yourself plagued with it. First, refresh immediately
+      and see if that fixes the issue. If not, navigate to options, and change the theme from design to literally
+      anything else. And above all else, Godspeed. We can't afford to lose anymore viewers.`
+  },
+  {
+    id: "a132",
+    text: "If I have bad English, I'll study English until I have good English."
+  },
+  {
+    id: "a133",
+    text:
+      `Someone once told me that antimatter is gonna roll me. I ain't the sharpest atom in the shed. WELL, the
+      tubes start coming and they don't stop coming...`
+  },
+  
+  {
+    id: "l71",
+    text: "Other languages await... I need to become a programmer",
+  },
 ] as const satisfies {
     id: string;
     text: string;
