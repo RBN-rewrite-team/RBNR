7df4654e81939f338059f2f173ca5bb22bbd9099
0100000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-12-13 00:01:25 +0800

    h

---
+++ src/core/post-nonrec/cardinal/pre-cardinal.ts
@@ -0,0 +1,13 @@
+export const LargeOrdList = [
+  [0, "α\\mapstoψ_Z\\left(α\\right)~\\mathrm{fp.}"],
+  [333, "\\mathrm{PTO}\\left(\\left(Π^2_1-CA\\right)_0\\right)"],
+  [344, "\\mathrm{PTO}\\left(Z_3\\right)"],
+  [350, "\\mathrm{PTO}\\left(Z_∞\\right)"],
+  [355, "\\mathrm{PTO}\\left(ZC\\right)"],
+  [360, "\\mathrm{PTO}\\left(\\mathrm{NFU}+\\left(\\mathrm{ Infinity }\\right)+\\left(\\mathrm{ Choice }\\right)\\right)"],
+  [362, "\\mathrm{PTO}\\left(\\mathrm{NFU}^*\\right)"],
+  [366, "\\mathrm{PTO}\\left(\\mathrm{ZF})"],
+  [370, "\\mathrm{PTO}\\left(\\mathrm{ZFC})"],
+  [400, "Ω"],
+  [633, "ω_1"],
+]
