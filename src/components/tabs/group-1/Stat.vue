7f65d04bdf6751a8336110d650be1e91048ed3a9
0100000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-12-13 21:24:29 +0800

    h

--- src/components/tabs/group-1/Stat.vue
+++ src/components/tabs/group-1/Stat.vue
@@ -17,7 +17,7 @@ import CenterLine from '@/components/ui/CenterLine.vue';
 import { currencyName, Currencies } from '@/core/currencies.ts';
 import { useI18n } from 'vue-i18n';
 import { i18n } from '@/utils/i18n.ts';
-import { preCardinalSpeed } from '@/core/game-loop.ts';
+import { preCardinalSpeed, diff } from '@/core/game-loop.ts';
 const $t = useI18n().t;
 const LunarMap = {
 	New: '新月',
@@ -135,6 +135,7 @@ function phase() {
 		</p>
 		<p>UUID: {{ player.uuid }}</p>
 		<p :key="updateKey" v-html="phase()"></p>
+		<p>TPS: {{ format(1000 / diff) }}, MSPT: {{ diff }}</p>
 
 		<p>
 			{{
