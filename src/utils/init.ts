109ff4f0109471d590aa9bf97798adf8da5bebbf
0100000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-12-14 11:26:34 +0800

    h

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -111,7 +111,8 @@ export function init() {
 				import.meta.env.DEV ? 0 : 2000,
 			);
 		});
-		detector = new MultiTabDetector({
+		let isSingleTab = true
+		if (false)detector = new MultiTabDetector({
 			heartbeatInterval: 3000,
 			onMultipleTabs: (result) => {
 				// isMultiTab.value = result.isMultiTab;
@@ -120,20 +121,24 @@ export function init() {
 				//   options.onDetection(result);
 				// }
 				console.log('Detected multi tabs');
-				ModalService.show({
-					title: getMessage('detectedmulti.title'),
-					content: getMessage('detectedmulti.content'),
-				});
-				stopSaveLoop();
+				if (isSingleTab) {
+				  ModalService.show({
+					  title: getMessage('detectedmulti.title'),
+					  content: getMessage('detectedmulti.content'),
+				  });
+				  stopSaveLoop();
+				  isSingleTab = false
+				}
 			},
 			onSingleTab: (result) => {
 				console.log("There\'s no wrong");
 				// isMultiTab.value = false;
 				// tabCount.value = 1;
 				startSaveLoop();
+				isSingleTab = true
 			},
 		});
-		detector.detect();
+		if (false)detector.detect();
 
 		// console.log(JSON.stringify(localization));
 	} catch (e) {
