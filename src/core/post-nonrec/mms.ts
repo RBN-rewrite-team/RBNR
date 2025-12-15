078eaa45d3fd017e5017bbc9187cfaebee028519
VeryrrDefine <1308061599@qq.com>
2025-12-15 22:07:01 +0800

    w

--- src/core/post-nonrec/mms.ts
+++ src/core/post-nonrec/mms.ts
@@ -1,1 +1,13 @@
-export const MMS = {} as const;
+/**
+ * player data put at hydra inner
+ */
+
+import PowiainaNum from 'powiaina_num.js';
+
+export const MMS = {
+	playerData() {
+		return {
+			deduced: new PowiainaNum(0),
+		};
+	},
+} as const;
