012e288eea033fe27ec361e1237de02f6343df01
VeryrrDefine <1308061599@qq.com>
2025-12-20 21:32:54 +0800

    how

--- src/core/anti-cheat.ts
+++ src/core/anti-cheat.ts
@@ -8,21 +8,21 @@ import { isDeveloper } from './save/testing.ts';
 import { buyables, upgrades } from './mechanic.ts';
 import PowiainaNum from 'powiaina_num.js';
 
-DisableDevtool({
-	onDevtoolOpen(type, next) {
-		stopGameLoop();
-		stopSaveLoop();
-		document.body.innerHTML = 'Malicious cheating behavior detected.';
+// DisableDevtool({
+// 	onDevtoolOpen(type, next) {
+// 		stopGameLoop();
+// 		stopSaveLoop();
+// 		document.body.innerHTML = 'Malicious cheating behavior detected.';
 
-		next();
-	},
-	rewriteHTML: 'Malicious cheating behavior detected.',
-	interval: 200,
-	md5: 'df17d82024bd335488f86e0c9c4ed23c',
-	url: 'https://localhost',
-	ignore: isDeveloper,
-	detectors: [0, 1, 3, 4, 6, 7], // Debugger容易被利用
-});
+// 		next();
+// 	},
+// 	rewriteHTML: 'Malicious cheating behavior detected.',
+// 	interval: 200,
+// 	md5: 'df17d82024bd335488f86e0c9c4ed23c',
+// 	url: 'https://localhost',
+// 	ignore: isDeveloper,
+// 	detectors: [0, 1, 3, 4, 6, 7], // Debugger容易被利用
+// });
 declare global {
 	interface Window {
 		game: object;
