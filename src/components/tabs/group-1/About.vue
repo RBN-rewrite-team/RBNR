ce408b185aeef3fd1a2e8b2a4933fc42d7b6b61b
2157436927 <52272599+dlsdl@users.noreply.github.com>
2025-12-21 13:54:58 +0800

    何意味

--- src/components/tabs/group-1/About.vue
+++ src/components/tabs/group-1/About.vue
@@ -122,6 +122,7 @@ import { i18n } from '@/utils/i18n';
 				<h2>{{ $t('about.mechanismdesign') }}</h2>
 				<br />
 				RAXH<br />
+				{{ $t('about.dlsdl') }}<br />
 				Seanxlx<br />
 			</div>
 			<div
