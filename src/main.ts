bb23d670d62a9b107efc1c7daef00ce5708e36e5
VeryrrDefine <1308061599@qq.com>
2025-12-27 15:43:02 +0800

    deleted performance

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,8 @@ PowiainaNum.throwErrorOnResultNaN = true;
 import './core/anti-cheat.ts';
 import './core/automator/evaluator.ts';
 import { init } from './utils/init.ts';
-import { Performance } from './core/performance.tsx';
 import './core/save/index.ts';
 import './core/hydra/hydra.ts';
-//import './utils/fffz.ts';
-import { Logarithm } from './core/exponention/logarithm';
 
 declare global {
 	interface BigInt {
@@ -21,38 +18,5 @@ BigInt.prototype.toJSON = function () {
 };
 init();
 
-import { achievements } from './core/achievements.ts';
-import { milestones } from './core/mechanic.ts';
-
 console.log('我错了', new PowiainaNum(3));
-// const res = {};
-// for (let i = 0; i < achievements.length; i++) {
-// 	for (let j = 0; j < achievements[i].length; j++) {
-// 		res[`ach.${i}.${j}`] = achievements[i][j].title;
-// 		res[`ach.${i}.${j}.desc`] = achievements[i][j].desc;
-// 	}
-// }
-// console.log(JSON.stringify(res));
-
-// window.exp = function () {
-// 	const res = {};
-// 	const a = milestones;
-// 	for (const i in a) {
-// 		const t = Object.getOwnPropertyDescriptor(a[i], 'description');
-// 		if (typeof t?.value !== 'undefined' && t.value !== null) {
-// 			res[i] = t.value;
-// 		} else {
-// 			res[i] = t.get;
-// 		}
-// 	}
-// 	return res;
-// };
-
-import { difference, xor } from 'lodash-es';
-
 import './test.tsx';
-import Decimal from 'break_eternity.js';
-import { Pow } from './core/automator/lexer.ts';
-import { format } from './utils/format.ts';
-import { addNotify } from './components/notify/index.ts';
-// window.addNotify = addNotify
