fdaca29c91c65cd65c70a1df90b90097e40bcf15
0100000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-12-27 19:05:31 +0800

    h

--- src/components/tabs/fffz/FFFZDeduction.tsx
+++ src/components/tabs/fffz/FFFZDeduction.tsx
@@ -1,7 +1,7 @@
 import { defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { FFFZmacros, FFFZMilestones } from '../../../utils/fffz.ts';
-import styles from './styles.scss';
+import styles from './styles.module.scss';
 import { VueLatex } from 'vatex';
 
 export default defineComponent({
@@ -11,20 +11,12 @@ export default defineComponent({
 			<>
 				<div>
 					<div
-						style={{
-							backgroundColor: 'var(--background-color)',
-							color: 'rgba(116, 155, 233, 1)',
-							height: '250px',
-							border: '2px solid',
-							borderImage: `linear-gradient(135deg, #1a6c5e, #1fb286, #ef2dfd) 1`,
-							position: 'relative',
-							display: 'block',
-							margin: 'auto',
-							zIndex: '1',
-							width: '50%',
+						class={{
+						  [styles.deduction_container]: true
 						}}
-					></div>
+					>
 					<VueLatex expression="test" macros={FFFZmacros} />
+					</div>
 				</div>
 			</>
 		);
