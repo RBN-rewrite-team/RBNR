64d81670b6010c1485275b1cb50fe5a36e19d051
0100000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2025-12-27 17:18:42 +0800

    h

--- src/components/tabs/fffz/FFFZDeduction.tsx
+++ src/components/tabs/fffz/FFFZDeduction.tsx
@@ -2,6 +2,7 @@ import { defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { FFFZmacros, FFFZMilestones } from '../../../utils/fffz.ts';
 import styles from './styles.scss';
+import { VueLatex } from 'vatex';
 
 export default defineComponent({
 	name: 'FFFZDeduction',
