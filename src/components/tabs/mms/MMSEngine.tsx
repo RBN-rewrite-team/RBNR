d558887b4f613ba5acb116cf149a90b4d72d7c37
seanxlx2011 <160576116+seanxlx2011@users.noreply.github.com>
2025-12-28 11:59:42 +0800

    Update MMSEngine.tsx

--- src/components/tabs/mms/MMSEngine.tsx
+++ src/components/tabs/mms/MMSEngine.tsx
@@ -220,7 +220,7 @@ export default defineComponent({
 					<br />
 					{MMS.rank.rankMilestones[0].map(
 						(x) =>
-							x[0].lte(player.hydra.mms.rank) &&
+							x[0].lte(x[2].includes('perm.') ? player.hydra.mms.bestRank : player.hydra.mms.rank) &&
 							tagAccord(tag.value, x[2]) &&
 							milestoneDisplay(x, MMS.rank.getRankTierName(0)),
 					)}
@@ -233,7 +233,7 @@ export default defineComponent({
 					)}
 					{MMS.rank.rankMilestones[1].map(
 						(x) =>
-							x[0].lte(player.hydra.mms.tier) &&
+							x[0].lte(x[2].includes('perm.') ? player.hydra.mms.bestTier : player.hydra.mms.tier) &&
 							tagAccord(tag.value, x[2]) &&
 							milestoneDisplay(x, MMS.rank.getRankTierName(1)),
 					)}
@@ -246,7 +246,7 @@ export default defineComponent({
 					)}
 					{MMS.rank.rankMilestones[2].map(
 						(x) =>
-							x[0].lte(player.hydra.mms.tri) &&
+							x[0].lte(x[2].includes('perm.') ? player.hydra.mms.bestTri : player.hydra.mms.tri) &&
 							tagAccord(tag.value, x[2]) &&
 							milestoneDisplay(x, MMS.rank.getRankTierName(2)),
 					)}
@@ -259,7 +259,7 @@ export default defineComponent({
 					)}
 					{MMS.rank.rankMilestones[3].map(
 						(x) =>
-							x[0].lte(player.hydra.mms.tetr) &&
+							x[0].lte(x[2].includes('perm.') ? player.hydra.mms.bestTetr : player.hydra.mms.tetr) &&
 							tagAccord(tag.value, x[2]) &&
 							milestoneDisplay(x, MMS.rank.getRankTierName(3)),
 					)}
